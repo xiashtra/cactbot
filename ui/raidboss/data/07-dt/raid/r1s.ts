374ce513d508901da73736c8d33e92e537bd8754
Legends0 <nac10fsu@gmail.com>
2024-07-31 18:25:44 -0400

    raidboss: r1s left/right, tb, and aoe triggers (#293)
     - Left/right call for boss when not leaping
     - TB callout
     - Big AoE callout for Bloody Scratch
    
    ---------
    
    Co-authored-by: xiashtra <91220277+xiashtra@users.noreply.github.com>

--- ui/raidboss/data/07-dt/raid/r1s.ts
+++ ui/raidboss/data/07-dt/raid/r1s.ts
@@ -1,3 +1,4 @@
+import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
 import { RaidbossData } from '../../../../../types/data';
 import { TriggerSet } from '../../../../../types/trigger';
@@ -8,7 +9,34 @@ const triggerSet: TriggerSet<Data> = {
   id: 'AacLightHeavyweightM1Savage',
   zoneId: ZoneId.AacLightHeavyweightM1Savage,
   timelineFile: 'r1s.txt',
-  triggers: [],
+  triggers: [
+    {
+      id: 'R1S One-two Paw Right Left',
+      type: 'StartsUsing',
+      netRegex: { id: '9436', source: 'Black Cat', capture: false },
+      durationSeconds: 9.5,
+      response: Responses.goLeftThenRight(),
+    },
+    {
+      id: 'R1S One-two Paw Left Right',
+      type: 'StartsUsing',
+      netRegex: { id: '9439', source: 'Black Cat', capture: false },
+      durationSeconds: 9.5,
+      response: Responses.goRightThenLeft(),
+    },
+    {
+      id: 'R1S Biscuit Maker',
+      type: 'StartsUsing',
+      netRegex: { id: '9495', source: 'Black Cat', capture: true },
+      response: Responses.tankBuster(),
+    },
+    {
+      id: 'R1S Bloody Scratch',
+      type: 'StartsUsing',
+      netRegex: { id: '9494', source: 'Black Cat', capture: false },
+      response: Responses.bigAoe(),
+    },
+  ],
 };
 
 export default triggerSet;
