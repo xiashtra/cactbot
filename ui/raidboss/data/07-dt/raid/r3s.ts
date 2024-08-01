b214f6b680bbda8acd478bcb5a058513a9f7cfb4
Akurosia Kamo <kamothegamer@web.de>
2024-08-01 17:51:12 +0200

    i18n: R3S translations (#302)

--- ui/raidboss/data/07-dt/raid/r3s.ts
+++ ui/raidboss/data/07-dt/raid/r3s.ts
@@ -43,6 +43,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Out + Spread',
+          de: 'Raus + Verteilen',
         },
       },
     },
@@ -54,6 +55,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'In + Spread',
+          de: 'Rein + Verteilen',
         },
       },
     },
@@ -65,6 +67,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Away + Spread',
+          de: 'Weg + Verteilen',
         },
       },
     },
@@ -76,6 +79,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Knockback + Spread',
+          de: 'Rückstoß + Verteilen',
         },
       },
     },
@@ -92,6 +96,7 @@ const triggerSet: TriggerSet<Data> = {
         getBehind: Outputs.getBehind,
         getHit: {
           en: 'Get hit by mist',
+          de: 'Werde vom Nebel getroffen',
         },
       },
     },
@@ -103,6 +108,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Away + Partners',
+          de: 'Weg + Partner',
         },
       },
     },
@@ -114,6 +120,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Knockback + Partners',
+          de: 'Rückstoß + Partner',
         },
       },
     },
@@ -125,6 +132,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Out + Partners',
+          de: 'Raus + Partner',
         },
       },
     },
@@ -136,6 +144,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'In + Partners',
+          de: 'Rein + Partner',
         },
       },
     },
@@ -148,6 +157,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Short Fuse',
+          de: 'Kurze Lunte',
         },
       },
     },
@@ -160,6 +170,7 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         text: {
           en: 'Long Fuse',
+          de: 'Lange Lunte',
         },
       },
     },
@@ -176,10 +187,130 @@ const triggerSet: TriggerSet<Data> = {
       outputStrings: {
         short: {
           en: 'Short Fuse',
+          de: 'Kurze Lunte',
         },
         long: {
           en: 'Long Fuse',
+          de: 'Lange Lunte',
         },
+      },
+    },
+  ],
+  timelineReplace: [
+    {
+      'locale': 'de',
+      'replaceSync': {
+        'Brute Bomber': 'Brutalo Bomber',
+        'Brute Distortion': 'Brutalo Bomber-Phantom',
+        'Lit Fuse': 'Zündschnurbombe',
+      },
+      'replaceText': {
+        '\\(cast\\)': '(wirken)',
+        '\\(damage\\)': '(Schaden)',
+        '\\(enrage\\)': '(Finalangriff)',
+        'Barbarous Barrage': 'Brutalo-Bomben',
+        'Blazing Lariat': 'Flammende Lariat',
+        'Bombarian Flame': 'Bomben-Feuer',
+        '(?<! )Bombarian Special': 'Brutalo-Spezial',
+        'Bombariboom': 'Brutalo-Schockwelle',
+        'Brutal Impact': 'Knallender Impakt',
+        'Chain Deathmatch': 'Ketten-Todeskampf',
+        'Diveboom': 'Bombensturz',
+        'Doping Draught': 'Aufputschen',
+        'Explosion': 'Explosion',
+        'Explosive Rain': 'Bombenregen',
+        'Final Fusedown': 'Epische Zündschnurbomben',
+        'Fuse or Foe': 'Klebrige Bombe',
+        'Fusefield': 'Luntenfeld',
+        'Fuses of Fury': 'Zündschnurbomben',
+        'Infernal Spin': 'Ultimativer Feuertornado',
+        'Knuckle Sandwich': 'Knöchelschlag',
+        'Lariat Combo': 'Lariat-Kombination',
+        'Murderous Mist': 'Grüner Nebel',
+        'Octoboom Bombarian Special': 'Okto-Brutalo-Spezial',
+        'Octoboom Dive': 'Okto-Bombensturz',
+        'Octuple Lariat': 'Okto-Lariat',
+        'Quadroboom Dive': 'Quattro-Bombensturz',
+        'Quadruple Lariat': 'Quattro-Lariat',
+        'Self-Destruct': 'Selbstzerstörung',
+        'Special Bombarian Special': 'Ultimativer Brutalo-Spezial',
+        'Tag Team': 'Wechselspiel',
+      },
+    },
+    {
+      'locale': 'fr',
+      'missingTranslations': true,
+      'replaceSync': {
+        'Brute Bomber': 'Brute Bomber',
+        'Brute Distortion': 'double de Brute Bomber',
+        'Lit Fuse': 'bombo à mèche',
+      },
+      'replaceText': {
+        'Barbarous Barrage': 'Bombardement brutal',
+        'Blazing Lariat': 'Lariat embrasé',
+        'Bombarian Flame': 'Feu brutal',
+        '(?<! )Bombarian Special': 'Spéciale brutale',
+        'Bombariboom': 'Onde de choc brutale',
+        'Brutal Impact': 'Impact brutal',
+        'Chain Deathmatch': 'Chaîne de la mort',
+        'Diveboom': 'Explongeon',
+        'Doping Draught': 'Dopage',
+        'Explosion': 'Explosion',
+        'Explosive Rain': 'Pluie explosive',
+        'Final Fusedown': 'Bombos à méche sadiques',
+        'Fuse or Foe': 'Fixation de bombo à mèche',
+        'Fusefield': 'Champs de mèches',
+        'Fuses of Fury': 'Bombos à mèche',
+        'Infernal Spin': 'Toupie infernale',
+        'Knuckle Sandwich': 'Sandwich de poings',
+        'Lariat Combo': 'Combo de lariats',
+        'Murderous Mist': 'Vapeur venimeuse',
+        'Octoboom Bombarian Special': 'Octuple spéciale brutale',
+        'Octoboom Dive': 'Octuple explongeon',
+        'Octuple Lariat': 'Octuple lariat',
+        'Quadroboom Dive': 'Quadruple explongeon',
+        'Quadruple Lariat': 'Quadruple lariat',
+        'Self-Destruct': 'Auto-destruction',
+        'Special Bombarian Special': 'Spéciale brutale ultime',
+        'Tag Team': 'Combat d\'équipe',
+      },
+    },
+    {
+      'locale': 'ja',
+      'missingTranslations': true,
+      'replaceSync': {
+        'Brute Bomber': 'ブルートボンバー',
+        'Brute Distortion': 'ブルートボンバーの幻影',
+        'Lit Fuse': 'フューズボム',
+      },
+      'replaceText': {
+        'Barbarous Barrage': 'ボンバリアンボム',
+        'Blazing Lariat': 'ラリアット・ブレイザー',
+        'Bombarian Flame': 'ボンバリアンファイヤー',
+        '(?<! )Bombarian Special': 'ボンバリアンスペシャル',
+        'Bombariboom': 'ボンバリアン・ショック',
+        'Brutal Impact': 'スマッシュインパクト',
+        'Chain Deathmatch': 'チェーンデスマッチ',
+        'Diveboom': 'パワーダイブ・ショック',
+        'Doping Draught': 'ドーピング',
+        'Explosion': '爆発',
+        'Explosive Rain': 'ボムレイン',
+        'Final Fusedown': '零式フューズボム',
+        'Fuse or Foe': 'アタッチ・フューズボム',
+        'Fusefield': 'フューズフィールド',
+        'Fuses of Fury': 'フューズボム',
+        'Infernal Spin': '極盛り式スピニングファイヤー',
+        'Knuckle Sandwich': 'ナックルパート',
+        'Lariat Combo': 'ラリアットコンビネーション',
+        'Murderous Mist': 'グリーンミスト',
+        'Octoboom Bombarian Special': '8ショック・ボンバリアンスペシャル',
+        'Octoboom Dive': '8ショック・パワーダイブ',
+        'Octuple Lariat': '8ウェイ・ダブルラリアット',
+        'Quadroboom Dive': '4ショック・パワーダイブ',
+        'Quadruple Lariat': '4ウェイ・ダブルラリアット',
+        'Self-Destruct': '自爆',
+        'Special Bombarian Special': 'アルティメット・ボンバリアンスペシャル',
+        'Tag Team': 'タッグマッチ',
       },
     },
   ],
