176e11072d9a2226cb86cd401ba066dd331c45c2
valarnin <valarnin@gmail.com>
2024-08-03 02:03:41 -0400

    raidboss: r3s add fooboom Special triggers (#309)

--- ui/raidboss/data/07-dt/raid/r3s.ts
+++ ui/raidboss/data/07-dt/raid/r3s.ts
@@ -9,6 +9,9 @@ export interface Data extends RaidbossData {
   phaseTracker: number;
 }
 
+// TODO: Lariat Combo during second KB towers?
+// TODO: <foo>boom Special delayed in/out triggers?
+
 const triggerSet: TriggerSet<Data> = {
   id: 'AacLightHeavyweightM3Savage',
   zoneId: ZoneId.AacLightHeavyweightM3Savage,
@@ -218,6 +221,28 @@ const triggerSet: TriggerSet<Data> = {
           de: 'Lange Lunte',
           ja: '長い導火線',
           cn: '长引线',
+        },
+      },
+    },
+    {
+      id: 'R3S Octoboom Bombarian Special',
+      type: 'StartsUsing',
+      netRegex: { id: '9752', source: 'Brute Bomber', capture: false },
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Out => In => Knockback => Spread',
+        },
+      },
+    },
+    {
+      id: 'R3S Quadroboom Bombarian Special',
+      type: 'StartsUsing',
+      netRegex: { id: '940A', source: 'Brute Bomber', capture: false },
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Out => In => Knockback => Partners',
         },
       },
     },
