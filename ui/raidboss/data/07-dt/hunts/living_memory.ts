fbf70a0d080c9d3aafe2961d8e5866cca7ff12be
xiashtra <91220277+xiashtra@users.noreply.github.com>
2024-09-25 19:25:35 -0500

    raidboss: additional Cat's Eye Bloodshot Gaze trigger (#434)

--- ui/raidboss/data/07-dt/hunts/living_memory.ts
+++ ui/raidboss/data/07-dt/hunts/living_memory.ts
@@ -94,7 +94,7 @@ const triggerSet: TriggerSet<Data> = {
       },
     },
     {
-      id: 'Hunt Cat\'s Eye Bloodshot Gaze',
+      id: 'Hunt Cat\'s Eye Bloodshot Gaze + Look Away',
       type: 'StartsUsing',
       netRegex: { id: '9673', source: 'Cat\'s Eye', capture: false },
       alertText: (_data, _matches, output) => output.text!(),
@@ -105,6 +105,17 @@ const triggerSet: TriggerSet<Data> = {
           fr: 'Pack (ne regardez pas la cible)',
           cn: '分摊 (背对目标)',
           ko: '쉐어 (대상에게서 뒤돌기)',
+        },
+      },
+    },
+    {
+      id: 'Hunt Cat\'s Eye Bloodshot Gaze + Look Toward',
+      type: 'StartsUsing',
+      netRegex: { id: '9AF4', source: 'Cat\'s Eye', capture: false },
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Stack (face toward target)',
         },
       },
     },
