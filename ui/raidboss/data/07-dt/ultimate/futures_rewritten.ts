291ea8b38e77672fb5bcdb62d98b1f3defb43ff9
Jonathan Garber <linkthevaliant@gmail.com>
2024-12-08 11:13:03 -0500

    raidboss: Improvements to FRU P2 timeline (#537)
    This has been tested in-game. I adjusted some timings to better match
    what I saw across my logs to match `test_timeline` and added the
    remaining `targetable` entries. I also combined the Shining and Frost
    armor entries since those always go off simultaneously but the order in
    the log is random. Finally, I filled out some more data on the P2
    abilities in the ability list.
    
    Timings for intermission and beyond have not been modified.

--- ui/raidboss/data/07-dt/ultimate/futures_rewritten.ts
+++ ui/raidboss/data/07-dt/ultimate/futures_rewritten.ts
@@ -493,6 +493,8 @@ const triggerSet: TriggerSet<Data> = {
     {
       'locale': 'en',
       'replaceText': {
+        'Axe Kick/Scythe Kick': 'Axe/Scythe Kick',
+        'Shining Armor + Frost Armor': 'Shining + Frost Armor',
         'Sinbound Fire III/Sinbound Thunder III': 'Sinbound Fire/Thunder',
       },
     },
