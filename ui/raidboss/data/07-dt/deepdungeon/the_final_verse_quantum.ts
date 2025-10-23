d00c876e599c00f6db4aa56cfa5f2ad5eeead92e
xiashtra <91220277+xiashtra@users.noreply.github.com>
2025-10-23 00:31:03 -0500

    initial complete timeline

--- ui/raidboss/data/07-dt/deepdungeon/the_final_verse_quantum.ts
+++ ui/raidboss/data/07-dt/deepdungeon/the_final_verse_quantum.ts
@@ -1,4 +1,4 @@
-// import Conditions from '../../../../../resources/conditions';
+import Conditions from '../../../../../resources/conditions';
 import Outputs from '../../../../../resources/outputs';
 import { Responses } from '../../../../../resources/responses';
 import ZoneId from '../../../../../resources/zone_id';
@@ -7,20 +7,23 @@ import { TriggerSet } from '../../../../../types/trigger';
 
 // Pilgrim's Traverse The Final Verse Quantum
 // TODO: Q15-39
+// TODO: Light/Dark Vengeance refresh warning
+// TODO: Scourging Blaze (exaflares) safe spot
+// TODO: Bounds of Sin north/south or east/west dodge direction + in/out
+// TODO: Manifold Lashings laser left/right direction
 
 const headMarkerData = {
-  // lockon5_t0h
+  // vfx/lockon/eff/lockon5_t0h
   spinelashTarget: '0017',
-  // m0344trg_a0h
+  // vfx/lockon/eff/m0344trg_a0h
   lightPartnerStack: '004D',
-  // m0344trg_b0h
+  // vfx/lockon/eff/m0344trg_b0h
   darkPartnerStack: '004E',
-  // m0376trg_fire3_a0p
+  // vfx/lockon/eff/m0376trg_fire3_a0p
   searingChains: '0061',
-  // share_laser_3sec_0t
+  // vfx/lockon/eff/share_laser_3sec_0t
   lineStackMarker: '020F', // on boss
 } as const;
-console.assert(headMarkerData);
 
 export type Data = RaidbossData;
 
@@ -29,10 +32,37 @@ const triggerSet: TriggerSet<Data> = {
   zoneId: ZoneId.TheFinalVerseQuantum,
   timelineFile: 'the_final_verse_quantum.txt',
   comments: {
-    en: 'Q40 only',
+    en: 'Q40',
   },
-
+  timelineTriggers: [
+    {
+      id: 'Final Verse Quantum Abyssal Sun',
+      // instant cast
+      regex: /Abyssal Sun/,
+      beforeSeconds: 5,
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Get Towers => AoE + Bleed',
+        },
+      },
+    },
+  ],
   triggers: [
+    {
+      id: 'Final Verse Quantum Scourging Blaze',
+      type: 'StartsUsing',
+      netRegex: { id: 'AC56', source: 'Eminent Grief', capture: false },
+      suppressSeconds: 1,
+      alarmText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Avoid Exaflares',
+          ja: 'エクサフレアを避ける',
+          ko: '엑사플레어 피하기',
+        },
+      },
+    },
     {
       id: 'Final Verse Quantum Blade of First Light',
       type: 'StartsUsing',
@@ -49,7 +79,16 @@ const triggerSet: TriggerSet<Data> = {
       },
     },
     {
-      id: 'Final Verse Quantum Ball of Fire',
+      id: 'Final Verse Quantum Ball of Fire Bait',
+      type: 'StartsUsing',
+      netRegex: { id: ['AC41', 'AC49'], source: 'Eminent Grief', capture: false },
+      infoText: (_data, _matches, output) => output.baitPuddles!(),
+      outputStrings: {
+        baitPuddles: Outputs.baitPuddles,
+      },
+    },
+    {
+      id: 'Final Verse Quantum Ball of Fire Move',
       type: 'Ability',
       netRegex: { id: ['AC41', 'AC49'], source: 'Eminent Grief', capture: false },
       response: Responses.moveAway('alert'),
@@ -66,6 +105,31 @@ const triggerSet: TriggerSet<Data> = {
         text: {
           en: 'AoE + Stop Moving!',
           ko: '전체 공격 + 이동 멈추기!',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum Searing Chains Stack',
+      type: 'HeadMarker',
+      netRegex: { id: headMarkerData.searingChains, capture: true },
+      condition: Conditions.targetIsYou(),
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Stack Middle for Chains',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum Searing Chains Break',
+      // 11D3 = Searing Chains
+      type: 'GainsEffect',
+      netRegex: { effectId: '11D3', capture: true },
+      condition: Conditions.targetIsYou(),
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Break Chains => AoE + Bleed',
         },
       },
     },
@@ -100,6 +164,172 @@ const triggerSet: TriggerSet<Data> = {
       type: 'AddedCombatant',
       netRegex: { npcNameId: '14039', capture: false },
       response: Responses.killAdds(),
+    },
+    {
+      id: 'Final Verse Quantum Shackles of Greater Sanctity',
+      type: 'StartsUsing',
+      netRegex: { id: 'AF01', source: 'Devoured Eater', capture: false },
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Shackles',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum Hellish Earth',
+      type: 'StartsUsing',
+      netRegex: { id: 'AC78', source: 'Eminent Grief', capture: false },
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'AoE + Draw-in',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum Hellish Earth Tether',
+      type: 'Tether',
+      netRegex: { id: '0005', source: 'Eminent Grief', capture: true },
+      infoText: (data, matches, output) => {
+        const target = matches.target;
+        if (data.me === target)
+          return output.tetherOnYou!();
+        if (data.role === 'tank')
+          return output.tetherOn!({ player: data.party.member(target) });
+      },
+      outputStrings: {
+        tetherOnYou: {
+          en: 'Tether on YOU',
+        },
+        tetherOn: {
+          en: 'Tether on ${player}',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum Manifold Lashings',
+      type: 'StartsUsing',
+      netRegex: { id: 'AC7F', source: 'Eminent Grief', capture: false },
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: '3x Tankbuster + Bleed',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum Manifold Lashings Laser',
+      type: 'StartsUsing',
+      netRegex: { id: 'AC81', source: 'Eminent Grief', capture: false },
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Avoid Laser',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum Unholy Darkness',
+      type: 'StartsUsing',
+      netRegex: { id: ['AC84', 'AC90'], source: 'Devoured Eater', capture: false },
+      response: Responses.bleedAoe('alert'),
+    },
+    {
+      id: 'Final Verse Quantum Crime and Punishment',
+      type: 'StartsUsing',
+      netRegex: { id: 'AC85', source: 'Devoured Eater', capture: false },
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Get Dark Vengeance',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum First Sin Bearer',
+      type: 'Ability',
+      netRegex: { id: 'AC86', source: 'Devoured Eater', capture: true },
+      condition: Conditions.targetIsYou(),
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Sin Bearer on YOU',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum Sin Bearer Pass Warning',
+      // 11D7 = Sin Bearer
+      type: 'GainsEffect',
+      netRegex: { effectId: '11D7', capture: true },
+      condition: (data, matches) => {
+        const stackCount = parseInt(matches.count);
+        return data.me === matches.target && stackCount === 15;
+      },
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Pass Sin Bearer',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum Eminent Grief Drain Aether',
+      // AC61 = short cast
+      // AC62 = long cast
+      type: 'StartsUsing',
+      netRegex: { id: ['AC61', 'AC62'], source: 'Eminent Grief', capture: true },
+      delaySeconds: (_data, matches) =>
+        matches.id === 'AC61' ? 0 : parseFloat(matches.castTime) - 5,
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Get Light Vengeance',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum Devoured Eater Drain Aether',
+      // AC63 = short cast
+      // AC65 = long cast
+      type: 'StartsUsing',
+      netRegex: { id: ['AC63', 'AC65'], source: 'Devoured Eater', capture: true },
+      delaySeconds: (_data, matches) =>
+        matches.id === 'AC63' ? 0 : parseFloat(matches.castTime) - 4,
+      alertText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Get Dark Vengeance',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum Flameborn Spawn',
+      // xxxx = Flameborn
+      type: 'AddedCombatant',
+      netRegex: { npcNameId: 'xxxx', capture: false },
+      suppressSeconds: 1,
+      infoText: (_data, _matches, output) => output.text!(),
+      outputStrings: {
+        text: {
+          en: 'Bait Flameborn',
+        },
+      },
+    },
+    {
+      id: 'Final Verse Quantum Flameborn Self-Destruct',
+      type: 'StartsUsing',
+      netRegex: { id: 'AC8B', source: 'Flameborn', capture: false },
+      response: Responses.aoe(),
+    },
+  ],
+  timelineReplace: [
+    {
+      'locale': 'en',
+      'replaceText': {
+        'Ball of Fire/Chains of Condemnation': 'Ball/Chains',
+      },
     },
   ],
 };
