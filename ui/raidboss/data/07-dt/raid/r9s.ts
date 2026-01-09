8ded8d164e607b0b0de8521b2cfeb0ee7ed1d87e
Legends0 <nac10fsu@gmail.com>
2026-01-09 18:04:04 -0500

    raidboss: add initial r9s triggers and timeline (#929)
    Something I worked on a bit. Most of the triggers are just pulled from
    normal and tested in game.
    
    ---------
    
    Co-authored-by: xiashtra <91220277+xiashtra@users.noreply.github.com>

---
+++ ui/raidboss/data/07-dt/raid/r9s.ts
@@ -0,0 +1,405 @@
+import Conditions from '../../../../../resources/conditions';
+import Outputs from '../../../../../resources/outputs';
+import { Responses } from '../../../../../resources/responses';
+import { DirectionOutput16, Directions } from '../../../../../resources/util';
+import ZoneId from '../../../../../resources/zone_id';
+import { RaidbossData } from '../../../../../types/data';
+import { NetMatches } from '../../../../../types/net_matches';
+import { TriggerSet } from '../../../../../types/trigger';
+
+export interface Data extends RaidbossData {
+  flailPositions: NetMatches['StartsUsingExtra'][];
+  actorPositions: { [id: string]: { x: number; y: number; heading: number } };
+  bats: {
+    inner: DirectionOutput16[];
+    middle: DirectionOutput16[];
+    outer: DirectionOutput16[];
+  };
+  satisfiedCount: number;
+  hasHellAwaits: boolean;
+}
+
+const headMarkerData = {
+  // Vfx Path: com_share4a1
+  'multiHitStack': '0131',
+  // Vfx Path: tank_lockonae_0m_5s_01t
+  'tankbuster': '01D4',
+  // Vfx Path: lockon5_line_1p
+  'aetherletting': '028C',
+  // Tethers used in Hell in a Cell and Undead Deathmatch
+  'tetherClose': '0161',
+  'tetherFar': '0162',
+} as const;
+
+const center = {
+  x: 100,
+  y: 100,
+};
+
+const triggerSet: TriggerSet<Data> = {
+  id: 'AacHeavyweightM1Savage',
+  zoneId: ZoneId.AacHeavyweightM1Savage,
+  timelineFile: 'r9s.txt',
+  initData: () => ({
+    flailPositions: [],
+    actorPositions: {},
+    bats: { inner: [], middle: [], outer: [] },
+    satisfiedCount: 0,
+    hasHellAwaits: false,
+  }),
+  triggers: [
+    {
+      id: 'R9S ActorSetPos Tracker',
+      type: 'ActorSetPos',
+      netRegex: { id: '4[0-9A-Fa-f]{7}', capture: true },
+      run: (data, matches) =>
+        data.actorPositions[matches.id] = {
+          x: parseFloat(matches.x),
+          y: parseFloat(matches.y),
+          heading: parseFloat(matches.heading),
+        },
+    },
+    {
+      id: 'R9S ActorMove Tracker',
+      type: 'ActorMove',
+      netRegex: { id: '4[0-9A-Fa-f]{7}', capture: true },
+      run: (data, matches) =>
+        data.actorPositions[matches.id] = {
+          x: parseFloat(matches.x),
+          y: parseFloat(matches.y),
+          heading: parseFloat(matches.heading),
+        },
+    },
+    {
+      id: 'R9S Killer Voice',
+      type: 'StartsUsing',
+      netRegex: { id: 'B384', source: 'Vamp Fatale', capture: false },
+      response: Responses.aoe(),
+    },
+    {
+      id: 'R9S Satisfied Counter',
+      type: 'GainsEffect',
+      netRegex: { effectId: '1277', capture: true },
+      run: (data, matches) => data.satisfiedCount = parseInt(matches.count),
+    },
+    {
+      id: 'R9S Headmarker Tankbuster',
+      type: 'HeadMarker',
+      netRegex: { id: headMarkerData['tankbuster'], capture: true },
+      condition: Conditions.targetIsYou(),
+      alertText: (data, _matches, output) => {
+        if (data.satisfiedCount >= 8)
+          return output.bigTankCleave!();
+        return output.tankCleaveOnYou!();
+      },
+      outputStrings: {
+        tankCleaveOnYou: Outputs.tankCleaveOnYou,
+        bigTankCleave: {
+          en: 'Tank Cleave on YOU (Big)',
+        },
+      },
+    },
+    {
+      id: 'R9S Vamp Stomp',
+      type: 'StartsUsing',
+      netRegex: { id: 'B34A', source: 'Vamp Fatale', capture: false },
+      response: Responses.getOut(),
+    },
+    {
+      id: 'R9S Headmarker Party Multi Stack',
+      // TODO: Add boss debuff tracker and indicate count
+      type: 'HeadMarker',
+      netRegex: { id: headMarkerData['multiHitStack'], capture: true },
+      response: Responses.stackMarkerOn(),
+    },
+    {
+      id: 'R9S Bat Tracker',
+      type: 'ActorControlExtra',
+      netRegex: { id: '4[0-9A-Fa-f]{7}', category: '0197', param1: '11D1', capture: true },
+      run: (data, matches) => {
+        const moveRads = {
+          'inner': 1.5128,
+          'middle': 1.5513,
+          'outer': 1.5608,
+        } as const;
+        const actor = data.actorPositions[matches.id];
+        if (actor === undefined)
+          return;
+        const dist = Math.hypot(actor.x - center.x, actor.y - center.y);
+        const dLen = dist < 16 ? (dist < 8 ? 'inner' : 'middle') : 'outer';
+
+        const angle = Math.atan2(actor.x - center.x, actor.y - center.y);
+        let angleCW = angle - (Math.PI / 2);
+        if (angleCW < -Math.PI)
+          angleCW += Math.PI * 2;
+        let angleDiff = Math.abs(angleCW - actor.heading);
+        if (angleDiff > Math.PI * 1.75)
+          angleDiff = Math.abs(angleDiff - (Math.PI * 2));
+
+        const cw = angleDiff < (Math.PI / 2) ? 'cw' : 'ccw';
+        const adjustRads = moveRads[dLen];
+        let endAngle = angle + (adjustRads * ((cw === 'cw') ? -1 : 1));
+        if (endAngle < -Math.PI)
+          endAngle += Math.PI * 2;
+        else if (endAngle > Math.PI)
+          endAngle -= Math.PI * 2;
+
+        data.bats[dLen].push(
+          Directions.output16Dir[Directions.hdgTo16DirNum(endAngle)] ?? 'unknown',
+        );
+      },
+    },
+    {
+      id: 'R9S Blast Beat Inner',
+      type: 'ActorControlExtra',
+      netRegex: { id: '4[0-9A-Fa-f]{7}', category: '0197', param1: '11D1', capture: false },
+      delaySeconds: 4.1,
+      durationSeconds: 5.5,
+      suppressSeconds: 1,
+      infoText: (data, _matches, output) => {
+        const [dir1, dir2] = data.bats.inner;
+
+        return output.away!({
+          dir1: output[dir1 ?? 'unknown']!(),
+          dir2: output[dir2 ?? 'unknown']!(),
+        });
+      },
+      run: (data, _matches) => {
+        data.bats.inner = [];
+      },
+      outputStrings: {
+        ...Directions.outputStrings16Dir,
+        away: {
+          en: 'Away from bats ${dir1}/${dir2}',
+          fr: 'Loin des chauves-souris ${dir1}/${dir2}',
+          cn: '远离 ${dir1}、${dir2} 蝙蝠',
+          ko: '박쥐 피하기 ${dir1}/${dir2}',
+        },
+      },
+    },
+    {
+      id: 'R9S Blast Beat Middle',
+      type: 'ActorControlExtra',
+      netRegex: { id: '4[0-9A-Fa-f]{7}', category: '0197', param1: '11D1', capture: false },
+      delaySeconds: 9.7,
+      durationSeconds: 3.4,
+      suppressSeconds: 1,
+      infoText: (data, _matches, output) => {
+        const [dir1, dir2, dir3] = data.bats.middle;
+
+        return output.away!({
+          dir1: output[dir1 ?? 'unknown']!(),
+          dir2: output[dir2 ?? 'unknown']!(),
+          dir3: output[dir3 ?? 'unknown']!(),
+        });
+      },
+      run: (data, _matches) => {
+        data.bats.middle = [];
+      },
+      outputStrings: {
+        ...Directions.outputStrings16Dir,
+        away: {
+          en: 'Away from bats ${dir1}/${dir2}/${dir3}',
+          fr: 'Loin des chauves-souris ${dir1}/${dir2}/${dir3}',
+          cn: '远离 ${dir1}、${dir2}、${dir3} 蝙蝠',
+          ko: '박쥐 피하기 ${dir1}/${dir2}/${dir3}',
+        },
+      },
+    },
+    {
+      id: 'R9S Blast Beat Outer',
+      type: 'ActorControlExtra',
+      netRegex: { id: '4[0-9A-Fa-f]{7}', category: '0197', param1: '11D1', capture: false },
+      delaySeconds: 13.2,
+      durationSeconds: 3.4,
+      suppressSeconds: 1,
+      response: Responses.goMiddle(),
+      run: (data, _matches) => {
+        data.bats.outer = [];
+      },
+    },
+    {
+      id: 'R9S Sadistic Screech',
+      type: 'StartsUsing',
+      netRegex: { id: 'B333', source: 'Vamp Fatale', capture: false },
+      response: Responses.bigAoe(),
+    },
+    {
+      id: 'R9S Crowd Kill',
+      type: 'StartsUsing',
+      netRegex: { id: 'B33E', source: 'Vamp Fatale', capture: false },
+      response: Responses.bigAoe(),
+    },
+    {
+      id: 'R9S Insatiable Thirst',
+      type: 'StartsUsing',
+      netRegex: { id: 'B344', source: 'Vamp Fatale', capture: false },
+      response: Responses.bigAoe(),
+    },
+    {
+      id: 'R9S Finale Fatale (Small)',
+      type: 'StartsUsing',
+      netRegex: { id: 'B340', source: 'Vamp Fatale', capture: false },
+      response: Responses.aoe(),
+    },
+    {
+      id: 'R9S Finale Fatale (Big)',
+      type: 'StartsUsing',
+      netRegex: { id: 'B341', source: 'Vamp Fatale', capture: false },
+      response: Responses.bigAoe(),
+    },
+    {
+      id: 'R9S Headmarker Aetherletting',
+      type: 'HeadMarker',
+      netRegex: { id: headMarkerData['aetherletting'], capture: true },
+      condition: Conditions.targetIsYou(),
+      infoText: (_data, _matches, output) => output.aetherlettingOnYou!(),
+      outputStrings: {
+        aetherlettingOnYou: {
+          en: 'Aetherletting on YOU',
+        },
+      },
+    },
+    {
+      id: 'R9S Plummet',
+      type: 'StartsUsingExtra',
+      netRegex: { id: 'B38B', capture: true },
+      preRun: (data, matches) => {
+        data.flailPositions.push(matches);
+      },
+      infoText: (data, _matches, output) => {
+        const [flail1Match, flail2Match] = data.flailPositions;
+
+        if (flail1Match === undefined || flail2Match === undefined)
+          return;
+
+        const flail1X = parseFloat(flail1Match.x);
+        const flail1Y = parseFloat(flail1Match.y);
+        const flail2X = parseFloat(flail2Match.x);
+        const flail2Y = parseFloat(flail2Match.y);
+
+        const flail1Dir = Directions.xyToIntercardDirOutput(flail1X, flail1Y, center.x, center.y);
+        const flail2Dir = Directions.xyToIntercardDirOutput(flail2X, flail2Y, center.x, center.y);
+
+        const flail1Dist = Math.abs(flail1Y - center.y) < 10 ? 'near' : 'far';
+        const flail2Dist = Math.abs(flail1Y - center.y) < 10 ? 'near' : 'far';
+
+        return output.text!({
+          flail1Dir: output[flail1Dir]!(),
+          flail2Dir: output[flail2Dir]!(),
+          flail1Dist: output[flail1Dist]!(),
+          flail2Dist: output[flail2Dist]!(),
+        });
+      },
+      run: (data) => {
+        if (data.flailPositions.length < 2)
+          return;
+        data.flailPositions = [];
+      },
+      outputStrings: {
+        text: {
+          en: 'Flails ${flail1Dist} ${flail1Dir}/${flail2Dist} ${flail2Dir}',
+          fr: 'Fléaux ${flail1Dist} ${flail1Dir}/${flail2Dist} ${flail2Dir}',
+          cn: '刺锤 ${flail1Dist}${flail1Dir}、${flail2Dist}${flail2Dir}',
+          ko: '철퇴 ${flail1Dist} ${flail1Dir}/${flail2Dist} ${flail2Dir}',
+        },
+        near: {
+          en: 'Near',
+          de: 'Nah',
+          fr: 'proche',
+          ja: '近',
+          cn: '近',
+          ko: '가까이',
+          tc: '近',
+        },
+        far: {
+          en: 'Far',
+          de: 'Fern',
+          fr: 'loin',
+          ja: '遠',
+          cn: '远',
+          ko: '멀리',
+          tc: '遠',
+        },
+        ...Directions.outputStringsIntercardDir,
+      },
+    },
+    {
+      id: 'R9S Hell Awaits Gain Debuff Collector',
+      type: 'GainsEffect',
+      netRegex: { effectId: '127A', capture: true },
+      condition: Conditions.targetIsYou(),
+      run: (data) => {
+        data.hasHellAwaits = true;
+      },
+    },
+    {
+      id: 'R9S Hell Awaits Lose Debuff Collector',
+      type: 'GainsEffect',
+      netRegex: { effectId: '127A', capture: true },
+      condition: Conditions.targetIsYou(),
+      // Can't use the actual lose line, since it's after cast for 3rd spread/stack
+      delaySeconds: 13,
+      run: (data) => {
+        data.hasHellAwaits = false;
+      },
+    },
+    {
+      id: 'R9S Ultrasonic Spread',
+      type: 'StartsUsing',
+      netRegex: { id: 'B39C', source: 'Vamp Fatale', capture: false },
+      infoText: (data, _matches, outputs) => {
+        return outputs.text!({
+          avoid: data.hasHellAwaits ? `${outputs.avoid!()} ` : '',
+          mech: outputs.rolePositions!(),
+        });
+      },
+      outputStrings: {
+        rolePositions: Outputs.rolePositions,
+        avoid: {
+          en: 'Avoid',
+        },
+        text: {
+          en: '${avoid}${mech}',
+        },
+      },
+    },
+    {
+      id: 'R9S Ultrasonic Amp',
+      type: 'StartsUsing',
+      netRegex: { id: 'B39D', source: 'Vamp Fatale', capture: false },
+      infoText: (data, _matches, outputs) => {
+        return outputs.text!({
+          avoid: data.hasHellAwaits ? `${outputs.avoid!()} ` : '',
+          mech: outputs.stack!(),
+        });
+      },
+      outputStrings: {
+        stack: Outputs.getTogether,
+        avoid: {
+          en: 'Avoid',
+        },
+        text: {
+          en: '${avoid}${mech}',
+        },
+      },
+    },
+    {
+      id: 'R9S Undead Deathmatch',
+      type: 'StartsUsing',
+      netRegex: { id: 'B3A0', source: 'Vamp Fatale', capture: false },
+      response: Responses.getTowers('alert'),
+    },
+  ],
+  timelineReplace: [
+    {
+      'locale': 'en',
+      'replaceText': {
+        'Ultrasonic Spread/Ultrasonic Amp': 'Ultrasonic Spread/Amp',
+        'Ultrasonic Amp/Ultrasonic Spread': 'Ultrasonic Amp/Spread',
+      },
+    },
+  ],
+};
+
+export default triggerSet;
