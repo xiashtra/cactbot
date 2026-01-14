7006dbae225e3ed3d95a9685d16ad5bac49158e1
Jacob Keller <jacob.e.keller@intel.com>
2026-01-13 19:43:17 -0800

    raidboss: zelenia ex improvements (#909)
    I've been carrying these on a personal branch for some time, and I
    figured
    it would be good to submit these.
    
    First, the bloom 1 logic can be simplified to remove potential
    inconsistency
    and errors, since there are only two outcomes.
    
    Second, the Escelons Fall mechanic has multiple strategies used in PF
    back
    when it was new. The default can work for any strategy, but it would be
    nice
    if the callouts could be tailored to specific strategies. I added these
    here.
    
    The improvements may not be that useful since the fight is getting older
    and
    overgeared.. but I've had them and tested for some time and liked the
    changes.
    
    - **raidboss: simplify bloom 1 logic for Zelenia Ex**
    - **raidboss: add strategy for Zelenia Ex Escelons' Fall**

--- ui/raidboss/data/07-dt/trial/zelenia-ex.ts
+++ ui/raidboss/data/07-dt/trial/zelenia-ex.ts
@@ -152,8 +152,6 @@ const mapEffectData: MapEffectData = {
   },
 } as const;
 
-const mapEffectTiles: MapEffectTile[] = Object.keys(mapEffectData) as MapEffectTile[];
-
 const isTileLoc = (loc: string): loc is TileSlotsType => {
   return tileSlots.includes(loc as TileSlotsType);
 };
@@ -220,12 +218,14 @@ const defaultTileState = () => ({
 } as const);
 
 export interface Data extends RaidbossData {
+  readonly triggerSetConfig: {
+    escelonsFall: 'dpsIn' | 'supportFirst' | 'dpsFirst' | 'none';
+  };
   phase: Phase;
   tileState: {
     [loc in MapEffectTile]: 'unknown' | 'red' | 'grey';
   };
   escelonFallBaits: ('near' | 'far')[];
-  bloom1StartDir?: number;
   bloom4RoseDirNorth: boolean;
   bloom5FirstDirSafe: 'dirNE' | 'dirNW' | 'dirSE' | 'dirSW' | 'unknown';
   bloom5SecondDirSafe: 'dirNE' | 'dirNW' | 'dirSE' | 'dirSW' | 'unknown';
@@ -235,6 +235,32 @@ export interface Data extends RaidbossData {
 const triggerSet: TriggerSet<Data> = {
   id: 'RecollectionExtreme',
   zoneId: ZoneId.RecollectionExtreme,
+  config: [
+    {
+      id: 'escelonsFall',
+      name: {
+        en: 'Escelons Fall Strategy',
+      },
+      comment: {
+        en: `Strategy for resolving Escelons' Fall 1 and 3.
+
+            None - Just call the first bait.
+            DPS In - DPS always start in, Support always start out.
+            Support First - Support bait the first hit.
+            DPS First - DPS bait the first hit.`,
+      },
+      type: 'select',
+      options: {
+        en: {
+          'None': 'none',
+          'DPS In': 'dpsIn',
+          'Support First': 'supportFirst',
+          'DPS First': 'dpsFirst',
+        },
+      },
+      default: 'none',
+    },
+  ],
   timelineFile: 'zelenia-ex.txt',
   initData: () => ({
     escelonFallBaits: [],
@@ -307,51 +333,57 @@ const triggerSet: TriggerSet<Data> = {
         if (bait1 === undefined || bait2 === undefined)
           return;
 
+        let first = 'unknown';
+
+        if (data.triggerSetConfig.escelonsFall === 'dpsIn') {
+          if (data.role === 'tank' || data.role === 'healer') {
+            first = 'out';
+          } else {
+            first = 'in';
+          }
+        } else if (data.triggerSetConfig.escelonsFall === 'supportFirst') {
+          if (data.role === 'tank' || data.role === 'healer') {
+            first = bait1 === 'near' ? 'in' : 'out';
+          } else {
+            first = bait1 === 'near' ? 'out' : 'in';
+          }
+        } else if (data.triggerSetConfig.escelonsFall === 'dpsFirst') {
+          if (data.role === 'tank' || data.role === 'healer') {
+            first = bait1 === 'near' ? 'out' : 'in';
+          } else {
+            first = bait1 === 'near' ? 'in' : 'out';
+          }
+        } else {
+          first = bait1;
+        }
+
         if (bait1 === bait2) {
           return output.swapAfterFirst!({
-            first: output[bait1]!(),
+            first: output[first]!(),
           });
         }
         return output.swapAfterSecond!({
-          first: output[bait1]!(),
+          first: output[first]!(),
         });
       },
       outputStrings: {
         near: {
-          en: 'Near',
-          de: 'Nah',
-          fr: 'proche',
-          ja: '近',
-          cn: '近',
-          ko: '가까이',
-          tc: '近',
+          en: 'Near bait first',
         },
         far: {
-          en: 'Far',
-          de: 'Fern',
-          fr: 'loin',
-          ja: '遠',
-          cn: '远',
-          ko: '멀리',
-          tc: '遠',
+          en: 'Far bait first',
+        },
+        out: {
+          en: 'Start out',
+        },
+        in: {
+          en: 'Start in',
         },
         swapAfterFirst: {
-          en: '${first} bait first, Swap after first+third',
-          de: '${first} zuerst ködern, wechsel nach dem ersten + dritten',
-          fr: 'Bait ${first} d\'abord, échangez après le 1er et 3ème',
-          ja: '${first} を先に誘導 → 1・3回目後に交代',
-          cn: '先 ${first} 引导, 1、3刀后交换',
-          ko: '처음 ${first} 유도, 1, 3번째 이후 교대',
-          tc: '先 ${first} 引導, 1、3刀後交換',
+          en: '${first}, Swap after first+third',
         },
         swapAfterSecond: {
-          en: '${first} bait first, Swap after second',
-          de: '${first} zuerst ködern, wechsel nach dem zweiten',
-          fr: 'Bait ${first} d\'abord, échangez après le 2ème',
-          ja: '${first} を先に誘導 → 2回目後に交代',
-          cn: '先 ${first} 引导, 2刀后交换',
-          ko: '처음 ${first} 유도, 2번째 이후 교대',
-          tc: '先 ${first} 引導, 2刀後交換',
+          en: '${first}, Swap after second',
         },
       },
     },
@@ -510,58 +542,21 @@ const triggerSet: TriggerSet<Data> = {
       response: Responses.bigAoe(),
     },
     {
-      id: 'ZeleniaEx Bloom 1 Rotation Collector',
-      type: 'MapEffect',
-      netRegex: {
-        location: tileSlots,
-        flags: [bloomTileFlags.red, bloomTileFlags.grey, bloomTileFlags.greyToRed],
-        capture: false,
-      },
-      condition: (data) => data.phase === 'bloom1',
-      delaySeconds: 0.5,
-      suppressSeconds: 100,
-      run: (data) => {
-        let dirIdx = 1;
-        let foundGrey = false;
-
-        // Find the 1st inner tile clockwise that's grey
-        for (const key of mapEffectTiles) {
-          if (!key.includes('Inner'))
-            continue;
-          if (foundGrey) {
-            if (data.tileState[key] === 'red') {
-              // Special edge case, NNW is safe
-              dirIdx = 15;
-            } else {
-              dirIdx += 2;
-            }
-            break;
-          }
-
-          if (data.tileState[key] === 'grey') {
-            foundGrey = true;
-            continue;
-          }
-
-          dirIdx += 2;
-        }
-
-        data.bloom1StartDir = dirIdx;
-      },
-    },
-    {
-      id: 'ZeleniaEx Bloom 1 Rotation',
+      id: 'ZeleniaEx Bloom 1',
       type: 'HeadMarker',
       netRegex: { id: [headMarkerData.clockwise, headMarkerData.counterclockwise], capture: true },
-      infoText: (data, matches, output) =>
+      infoText: (_data, matches, output) =>
         output.text!({
-          dir: output[Directions.output16Dir[data.bloom1StartDir ?? -1] ?? 'unknown']!(),
+          dir: matches.id === headMarkerData.clockwise
+            ? output.dirSE!()
+            : output.dirN!(),
           rotate: matches.id === headMarkerData.clockwise
             ? output.clockwise!()
             : output.counterclockwise!(),
         }),
       outputStrings: {
-        ...Directions.outputStrings16Dir,
+        dirN: Outputs.dirN,
+        dirSE: Outputs.dirSE,
         clockwise: Outputs.clockwise,
         counterclockwise: Outputs.counterclockwise,
         text: {
