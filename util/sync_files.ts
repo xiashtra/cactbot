66f9abb722a3c992e2a52a771242d15e328a0885
Jonathan Garber <linkthevaliant@gmail.com>
2026-05-03 15:35:33 -0400

    raidboss/oopsy: Add Shinryu Unreal (#1041)
    Tested in-game. I do plan to work through the TODOs in the Oopsy file,
    but I think we're best off getting a functional triggerset merged for
    now and then revisiting things once the rest of the patch content is
    supported.

--- util/sync_files.ts
+++ util/sync_files.ts
@@ -983,6 +983,107 @@ const zoneReplace: ZoneReplace[] = [
       '2EB2': 'B16A', // Torment unto Death
     },
   },
+  { // Shinryu Unreal
+    fileMap: {
+      'ui/raidboss/data/04-sb/trial/shinryu-ex.ts': 'ui/raidboss/data/07-dt/trial/shinryu-un.ts',
+      'ui/raidboss/data/04-sb/trial/shinryu-ex.txt': 'ui/raidboss/data/07-dt/trial/shinryu-un.txt',
+      'ui/oopsyraidsy/data/04-sb/trial/shinryu-ex.ts':
+        'ui/oopsyraidsy/data/07-dt/trial/shinryu-un.ts',
+    },
+    prefix: { 'ShinryuEx': 'ShinryuUn' },
+    other: {
+      'TheMinstrelsBalladShinryusDomain': 'ShinryusDomainUnreal',
+      'Shinryu Extreme': 'Shinryu Unreal',
+      'shinryu-ex.txt': 'shinryu-un.txt',
+    },
+    // Primary offset 9E5C
+    // Secondary offset 9E2D
+
+    id: {
+      '1DD1': 'BC2D', // Tail Spit, not seen in current logs. Possibly normal mode?
+      '1DD3': 'BC2F', // Dragonflight, not seen in current logs. Possibly normal mode?
+      '1FA8': 'BFEB', // Gyre Charge, not seen in current logs. Possibly normal mode?
+      '1FF4': 'BFEC', // Gyre Charge, not seen in current logs. Possibly normal mode?
+      '25D7': 'C433', // Tail Spit
+      '25D9': 'C435', // Enhanced Aetheric Burst
+      '25DA': 'C436', // Tidal Wave
+      '25DB': 'C437', // Hellfire
+      '25DC': 'C438', // Judgment Bolt
+      '25DD': 'C439', // Diamond Dust
+      '25DE': 'C43A', // Earthen Fury
+      '25DF': 'C43B', // Aerial Blast
+      '25E0': 'C43C', // Enhanced Dexterity
+      '25E1': 'C43D', // All Fours
+      '25E2': 'C43E', // Tail Slap
+      '25E3': 'C43F', // Enhanced Sneak Attack
+      '25E4': 'C440', // Protostar
+      '25E5': 'C441', // Meteor Impact
+      '25E6': 'C442', // Explosive Hatch, not seen in current logs. Possibly normal mode or mechanical failure?
+      '25E7': 'C443', // Dark Matter
+      '25E8': 'C444', // Hypernova
+      '25E9': 'C445', // Hypernova
+      '25EA': 'C446', // Levinbolt
+      '25EB': 'C447', // Levinbolt
+      '25EC': 'C448', // Earth Breath
+      '25ED': 'C449', // Earth Breath
+      '25EE': 'C44A', // Summon Icicle
+      '25EF': 'C44B', // Icicle Impact
+      '25F0': 'C44C', // Spikesicle
+      '25F1': 'C44D', // Ice Storm
+      '25F2': 'C44E', // Ice Storm
+      '25F3': 'C44F', // Akh Morn
+      '25F4': 'C450', // Akh Morn
+      '25F5': 'C451', // Akh Rai
+      '25F6': 'C452', // Akh Rai
+      '25F7': 'C453', // Enhanced Stealth IV
+      '25F9': 'C455', // Tidal Wave
+      '25FA': 'C456', // Hellfire
+      '25FB': 'C457', // Judgment Bolt
+      '25FC': 'C458', // Diamond Dust
+      '25FD': 'C459', // Earthen Fury
+      '25FE': 'C45A', // Aerial Blast
+      '25FF': 'C45B', // Protostar
+      '2600': 'C45C', // Gulleye V
+      '2601': 'C45D', // Cloudfishing
+      '2602': 'C45E', // Burning Chains
+      '2603': 'C45F', // Gyre Charge
+      '2604': 'C460', // Enhanced Stealth IV
+      '2605': 'C461', // Unknown, possibly add hatches?
+      '2608': 'C464', // Spiked Tail
+      '2609': 'C465', // Blazing Trail
+      '260A': 'C466', // Death Sentence
+      '260B': 'C467', // Fireball
+      '260C': 'C468', // Super Cyclone
+      '260D': 'C469', // Tail Slap, not seen in modern logs. Possibly normal mode?
+      '260E': 'C46A', // Hasty Reprisal
+      '260F': 'C46B', // Meteor Impact
+      '2610': 'C46C', // Dragonfist
+      '2611': 'C46D', // Dragonfist
+      '2612': 'C46E', // Dragon Spirit
+      '2613': 'C46F', // Touchdown
+      '2614': 'C470', // Touchdown
+      '2615': 'C471', // Tail Spit
+      '2616': 'C472', // Tail Spit
+      '2617': 'C473', // Shatter
+      '2618': 'C474', // Aetherial Ray
+
+      // Secondary offset begins
+      '2648': 'C475', // Wormwail
+      '2649': 'C476', // Benighting Breath
+      '264A': 'C477', // Benighting Breath
+      '264B': 'C478', // Tera Slash
+      '264C': 'C479', // Atomic Ray
+      '264D': 'C47A', // Atomic Ray
+      '264E': 'C47B', // Tidal Wave Enrage
+      '2718': 'C47D', // Judgment Bolt Enrage
+      '2719': 'C47E', // Hellfire Enrage
+      '271F': 'C481', // Hypernova
+      '2720': 'C482', // Levinbolt
+      '2722': 'C484', // Ice Storm
+      '2723': 'C485', // Levinbolt
+      '2725': 'C486', // Levinbolt, not seen in modern logs. Possibly normal mode?
+    },
+  },
 ];
 
 const processFile = (filename: string, zone: ZoneReplace, inputText: string): string => {
